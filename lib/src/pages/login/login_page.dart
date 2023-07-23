42dac3d21fed3650b4523b006d0d3e92919144c0
anjimenez21 <81935849+anjimenez21@users.noreply.github.com>
2023-07-22 23:39:50 -0600

    Pantalla Principal Parte 1
    Creacion de Pantalla Principal Parte 1

--- lib/src/pages/login/login_page.dart
+++ lib/src/pages/login/login_page.dart
@@ -5,10 +5,84 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('LOGIN PAGE'),
+    return  Scaffold(
+      bottomNavigationBar: Container(
+        height: 50,
+        child: _textDontHaveAccount(),
+      ),
+      body: Stack( // POSICIONAR ELEMENTOS UNO ENCIMA DEL OTRO
+        children: [
+          _backgroundCover(context),
+          Column( // POSICIONAR ELEMENTOS UNO DEBAJO DEL OTRO (VERTICAL)
+            children: [
+              _imageCover(),
+              _textAppName()
+            ],
+          ),
+        ],
       ),
     );
   }
-}
+
+
+  Widget _backgroundCover(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height * 0.42,
+      color: Colors.amber,
+    );
+  }
+
+  Widget _textAppName() {
+    return const Text(
+      'APP PEDIDOS',
+      style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: Colors.black
+      ),
+    );
+  }
+
+  Widget _textDontHaveAccount() {
+    return Row( // UBICAR ELEMENTOS UNO AL LADO DEL OTRO (HORIZONTAL)
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          '¿No tienes cuenta?',
+          style: TextStyle(
+              color: Colors.black,
+              fontSize: 17
+          ),
+        ),
+        SizedBox(width: 7),
+        GestureDetector(
+          child: Text(
+            'Registrate Aqui',
+            style: TextStyle(
+                color: Colors.amber,
+                fontWeight: FontWeight.bold,
+                fontSize: 17
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _imageCover() {
+    return SafeArea(
+      child: Container(
+        margin: EdgeInsets.only(top: 20, bottom: 15),
+        alignment: Alignment.center,
+        child: Image.asset(
+          'assets/img/delivery.png',
+          width: 130,
+          height: 130,
+        ),
+      ),
+    ) ;
+  }
+
+
+  }
