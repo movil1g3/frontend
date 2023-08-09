296ad2f95b3b2eb50c35757054ab9ea466079171
Alfaken90 <archaga.kevin@gmail.com>
2023-08-08 23:45:34 -0600

    Actualizacion de la direccion IP

--- projet_delivery/lib/src/environment/environment.dart
+++ projet_delivery/lib/src/environment/environment.dart
@@ -1,7 +1,7 @@
 class Environment {
 
-  static const String API_URL  = "http://192.168.8.214:3000/";
-  static const String API_URL_OLD  = "192.168.8.214:3000";
+  static const String API_URL  = "http://192.168.0.4:3000/";
+  static const String API_URL_OLD  = "192.168.0.4:3000";
   static const String API_KEY_MAPS  = "AIzaSyCdcDaI1uIkhFFrdjjuai_lHbILSX6Zgd0";
 
 }
